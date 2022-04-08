4c72084452881c39117a2f10fa222f39d109aa71
alvin.wang <alvin.wang@binance.com>
2022-04-08 09:12:57 +0800

    chore: commit removeElement

---
+++ Practise/27-removeElement.js
@@ -0,0 +1,21 @@
+/**
+ * 解题思路: 双指针替换
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    if (nums[left] === val) {
+      nums[left] = nums[right];
+      right--;
+    } else {
+      left++;
+    }
+  }
+
+  return left;
+};
