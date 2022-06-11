c667699afcf9a7107483c64402ef59db661dea6d
Madman804 <alvin.wang1202@gmail.com>
2022-06-11 12:03:06 +0200

    fix: commit 81-search

---
+++ Practise/81-search.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left + ((right - left) >> 1);
+    if (nums[mid] === target) return true;
+
+    if (nums[mid] === nums[left]) {
+      ++left;
+      continue;
+    }
+
+    if (nums[mid] > nums[left]) {
+      // 左侧是生序
+      if (target >= nums[left] && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (target > nums[mid] && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return false;
+};
